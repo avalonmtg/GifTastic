66c8ab621bf11ea16ee44b718d1eadc924342352
JoanStoddard <joanstoddard16@gmail.com>
2020-03-10 21:26:45 -0700

    animate commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,6 +25,14 @@ $(document).ready( function() {
         return;
     });
 
+    
+    $("#add-flowers").on("click", function (event) {
+        var submit = $("#flowerInput").val().trim();
+        topics.push(submit);
+        renderButton();
+        $("#newFlower")[0].reset();
+    });
+//gifs
     $(document).on("click", ".flower", function() {
     //$(".flower").on("click", function () {
         var flower = $(this).attr("data-name");
@@ -32,17 +40,17 @@ $(document).ready( function() {
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             flower + "&api_key=zLZvQScybqQWuQIVCY9UHUgmAgc1Jzyx&limit=10&offset=0&rating=G&lang=en"
 
-        // Performing an AJAX request with the queryURL
+        //ajax
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-            // After data comes back from the request
+
             .then(function (response) {
                 console.log(queryURL);
 
                 console.log(response);
-                // storing the data from the AJAX request in the results variable
+                
                 var results = response.data;
 
                 $("#gifsDisplay").empty();
@@ -50,48 +58,48 @@ $(document).ready( function() {
                 // Looping through each result item
                 for (var i = 0; i < results.length; i++) {
 
-                    // Creating and storing a div tag
+                    //  div tag
                     var flowerDiv = $("<div>");
 
-                    // Creating a paragraph tag with the result item's rating
                     var p = $("<p>").text("Rating: " + results[i].rating);
 
-                    // Creating and storing an image tag
+                    // image tag
                     var flowerImage = $("<img>");
-                    // Setting the src attribute of the image to a property pulled off the result item
+                    
                     flowerImage.attr("src", results[i].images.original_still.url);
                     flowerImage.attr("data-still", results[i].images.original_still.url);
                     flowerImage.attr("data-animate", results[i].images.original.url);
                     flowerImage.attr("data-state", "still");
                     flowerImage.attr("class", "gif");
 
-                    // Appending the paragraph and image tag to the animalDiv
+                    
                     flowerDiv.append(p);
                     flowerDiv.append(flowerImage);
 
-                    // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+                
                     $("#gifsDisplay").append(flowerDiv);
                 }
             });
     });
-
+//animation
     function gifAnimation() {
 
         var state = $(this).attr("data-state");
-        var still = $(this).attr("data-still");
-        var animate = $(this).attr("data-animate");
-
+        var stillImage = $(this).attr("data-still");
+        var animateImage = $(this).attr("data-animate");
+        
         if (state === "still") {
-            $(this).attr("src", animate);
+            $(this).attr("src", animateImage);
             $(this).attr("data-state", "animate");
             console.log(state)
         }
         else if (state === animate) {
-            $(this).attr("src", still);
+            $(this).attr("src", stillImage);
             $(this).attr("data-state", "still");
             console.log(animate)
         }
     }
-
-    $(document).on("click", "gif", gifAnimation);
+    $(document).on("click", ".flower", gifsDisplay);
+    $(document).on("click", ".gif", gifAnimation);
+   
 })
